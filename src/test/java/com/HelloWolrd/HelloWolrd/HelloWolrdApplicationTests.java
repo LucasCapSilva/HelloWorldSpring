e49e3cb9c875438e5ef378599bb8d4a7f73975d0
unknown <lcapelotto@gmail.com>
2022-09-08 09:13:22 -0300

    hello world

---
+++ src/test/java/com/HelloWolrd/HelloWolrd/HelloWolrdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HelloWolrd.HelloWolrd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWolrdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
